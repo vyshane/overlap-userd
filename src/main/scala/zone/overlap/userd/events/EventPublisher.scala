6f05b5dc7ed1a61846f01c0510dfc06f33e06d3a
Vy-Shane Xie <shane@node.mu>
2017-11-26 22:57:36 +0800

    Add comment regarding log

--- src/main/scala/zone/overlap/userd/events/EventPublisher.scala
+++ src/main/scala/zone/overlap/userd/events/EventPublisher.scala
@@ -4,12 +4,17 @@ package zone.overlap.userd.events
 
 import com.typesafe.config.Config
 import monix.eval.Task
+import org.slf4j.LoggerFactory
 import zone.overlap.api.internal.events.user.UserSignedUp
 
 case class EventPublisher(config: Config) {
 
+  val log = LoggerFactory.getLogger(this.getClass)
   val bootstrapServers = config.getString("kafka.bootstrapServers")
 
   // TODO
-  def sendUserSignedUp(userSignedUp: UserSignedUp): Task[Unit] = ???
+  def sendUserSignedUp(userSignedUp: UserSignedUp): Task[Unit] = {
+    // If there's an error while sending to topic, log it
+    ???
+  }
 }
