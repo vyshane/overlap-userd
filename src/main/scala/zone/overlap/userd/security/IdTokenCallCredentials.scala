3259bfde48a8b3e2333f7d97ab6eee0e94cd39f7
Vy-Shane Xie <shane@node.mu>
2017-12-21 21:16:12 +0800

    Add token call credentials

---
+++ src/main/scala/zone/overlap/userd/security/IdTokenCallCredentials.scala
@@ -0,0 +1,25 @@
+// Copyright 2017 Vy-Shane Xie Sin Fat
+
+package zone.overlap.userd.security
+
+import java.util.concurrent.Executor
+
+import io.grpc.{Attributes, CallCredentials, Metadata, MethodDescriptor}
+
+class IdTokenCallCredentials(accessToken: String) extends CallCredentials {
+
+  override def applyRequestMetadata(method: MethodDescriptor[_, _],
+                                    attributes: Attributes,
+                                    appExecutor: Executor,
+                                    applier: CallCredentials.MetadataApplier): Unit = {
+    appExecutor.execute(() => {
+      val headers = new Metadata()
+      val authorizationHeaderKey =
+        Metadata.Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER)
+      headers.put(authorizationHeaderKey, "Bearer " + accessToken)
+      applier.apply(headers)
+    })
+  }
+
+  override def thisUsesUnstableApi() = ()
+}
