55bed81482a5059aaff393abe1d004cb04916990
Vy-Shane Xie <shane@node.mu>
2017-12-31 08:08:52 +0800

    Add update info endpoint test skeleton

--- src/test/scala/zone/overlap/api/endpoints/UpdateInfoEndpointSpec.scala
+++ src/test/scala/zone/overlap/api/endpoints/UpdateInfoEndpointSpec.scala
@@ -2,8 +2,20 @@
 
 package zone.overlap.api.endpoints
 
-class UpdateInfoEndpointSpec {
+import org.scalamock.scalatest.AsyncMockFactory
+import org.scalatest.{AsyncWordSpec, Matchers, RecoverMethods}
 
-  // TODO
+class UpdateInfoEndpointSpec extends AsyncWordSpec with AsyncMockFactory with Matchers with RecoverMethods {
 
+  "The updateInfo public endpoint" when {
+    "sent an unauthenticated request" should {
+      "raise an error with status unauthenticated" in (pending)
+    }
+    "sent an invalid request" should {
+      "raise an error containing the validation errors" in (pending)
+    }
+    "sent a valid request" should {
+      "update the user's info and return an update info response" in (pending)
+    }
+  }
 }
