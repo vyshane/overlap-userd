993ca166abd91cd9c56a435fd0f09ae8958fe114
Vy-Shane Xie <shane@node.mu>
2018-01-26 08:48:36 +0800

    Update user repository tests

--- src/test/scala/zone/overlap/TestUtils.scala
+++ src/test/scala/zone/overlap/TestUtils.scala
@@ -3,38 +3,57 @@
 package zone.overlap
 
 import java.time.Instant
+import java.util.UUID
 
 import com.github.javafaker.Faker
 import com.trueaccord.scalapb.{GeneratedEnum, GeneratedEnumCompanion}
+import monix.eval.Task
 import zone.overlap.privateapi.user.UserStatus
 import zone.overlap.userd.persistence.UserRecord
 import zone.overlap.userd.utils._
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
 import scala.util.Random
 
 object TestUtils {
 
   private val faker = new Faker
 
-  def randomEnum[T <: GeneratedEnum](enumCompanion: GeneratedEnumCompanion[T]) = {
+  import monix.execution.Scheduler.Implicits.global
+  def awaitResult[T](task: Task[T]): T = Await.result(task.runAsync, 5 seconds)
+
+  def randomEnum[T <: GeneratedEnum](enumCompanion: GeneratedEnumCompanion[T]): T = {
     enumCompanion.values.iterator.drop(Random.nextInt(enumCompanion.values.size)).next()
   }
 
-  def randomUserRecord(): UserRecord = {
-    randomUserRecord(faker.gameOfThrones().house())
+  def randomEnumExcept[T <: GeneratedEnum](enumCompanion: GeneratedEnumCompanion[T])(except: T): Option[T] = {
+    if (enumCompanion.values.size == 1) {
+      // There is no enum value other than the one the caller doesn't want
+      Option.empty
+    } else {
+      var enum: T = randomEnum(enumCompanion)
+      while (enum == except) enum = randomEnum(enumCompanion)
+      Option(enum)
+    }
   }
 
-  def randomUserRecord(userId: String): UserRecord = {
+  def randomVerifiedUserRecord(): UserRecord = {
     val firstName = faker.name().firstName()
     UserRecord(
-      userId,
+      UUID.randomUUID().toString,
       firstName,
       faker.name().lastName(),
       faker.internet().emailAddress(firstName.toLowerCase),
-      randomUniqueCode(),
-      hashPassword(faker.gameOfThrones().quote().toLowerCase()),
-      randomEnum(UserStatus.enumCompanion),
+      Option.empty,
+      hashPassword(faker.gameOfThrones().quote().toLowerCase().replace(" ", "")),
+      randomEnumExcept(UserStatus.enumCompanion)(UserStatus.PENDING_EMAIL_VERIFICATION).get.name,
       Instant.now()
     )
+  }
+
+  def randomPendingUserRecord(): UserRecord = {
+    randomVerifiedUserRecord()
+      .copy(status = UserStatus.PENDING_EMAIL_VERIFICATION.name, emailVerificationCode = Option(randomUniqueCode()))
   }
 }
