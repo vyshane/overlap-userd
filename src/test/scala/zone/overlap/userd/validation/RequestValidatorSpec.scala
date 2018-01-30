4783318c916c0699fb2e46522958c70298f656ad
Vy-Shane Xie <shane@node.mu>
2018-01-30 23:12:42 +0800

    Add request validator test skeleton

---
+++ src/test/scala/zone/overlap/userd/validation/RequestValidatorSpec.scala
@@ -0,0 +1,11 @@
+// Copyright 2018 Vy-Shane Xie Sin Fat
+
+package zone.overlap.userd.validation
+
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.{Matchers, WordSpec}
+
+class RequestValidatorSpec extends WordSpec with MockFactory with Matchers {
+
+  // TODO(SX)
+}
