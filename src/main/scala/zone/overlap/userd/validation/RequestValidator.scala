befb1d8c5827f4d7b20c40fbf55acff4e9150496
Vy-Shane Xie <shane@node.mu>
2018-01-30 20:34:37 +0800

    Move private functions to bottom

--- src/main/scala/zone/overlap/userd/validation/RequestValidator.scala
+++ src/main/scala/zone/overlap/userd/validation/RequestValidator.scala
@@ -13,6 +13,26 @@ sealed trait RequestValidator {
 
   type ValidationResult[A] = ValidatedNel[UserValidation, A]
 
+  def ensureValid[A](validator: A => ValidationResult[A])(value: A): Task[A] =
+    validator(value) match {
+      case Valid(value) => Task(value)
+      case Invalid(nel) => {
+        val errorDescription = nel.map(v => v.errorMessage).toList.mkString("\n")
+        Task.raiseError(Status.INVALID_ARGUMENT.augmentDescription(errorDescription).asRuntimeException())
+      }
+    }
+
+  def validateSignUpRequest(emailExists: String => Boolean)(request: SignUpRequest): ValidationResult[SignUpRequest] = {
+    (validateFirstName(request.firstName),
+     validateLastName(request.lastName),
+     validateEmail(emailExists)(request.email),
+     validatePassword(request.password)).mapN(SignUpRequest.apply)
+  }
+
+  def validateUpdateUserInfoRequest(request: UpdateInfoRequest): ValidationResult[UpdateInfoRequest] = {
+    (validateFirstName(request.firstName), validateLastName(request.lastName)).mapN(UpdateInfoRequest.apply)
+  }
+
   private def validateFirstName(firstName: String): ValidationResult[String] =
     if (firstName.length > 1 && firstName.length <= 255) firstName.validNel
     else FirstNameIsInvalid.invalidNel
@@ -37,26 +57,6 @@ sealed trait RequestValidator {
   private def validatePassword(password: String): ValidationResult[String] =
     if (password.length >= 6) password.validNel
     else PasswordIsTooShort.invalidNel
-
-  def ensureValid[T](validator: T => ValidationResult[T])(value: T): Task[T] =
-    validator(value) match {
-      case Valid(value) => Task(value)
-      case Invalid(nel) => {
-        val errorDescription = nel.map(v => v.errorMessage).toList.mkString("\n")
-        Task.raiseError(Status.INVALID_ARGUMENT.augmentDescription(errorDescription).asRuntimeException())
-      }
-    }
-
-  def validateSignUpRequest(emailExists: String => Boolean)(request: SignUpRequest): ValidationResult[SignUpRequest] = {
-    (validateFirstName(request.firstName),
-     validateLastName(request.lastName),
-     validateEmail(emailExists)(request.email),
-     validatePassword(request.password)).mapN(SignUpRequest.apply)
-  }
-
-  def validateUpdateUserInfoRequest(request: UpdateInfoRequest): ValidationResult[UpdateInfoRequest] = {
-    (validateFirstName(request.firstName), validateLastName(request.lastName)).mapN(UpdateInfoRequest.apply)
-  }
 }
 
 object RequestValidator extends RequestValidator
