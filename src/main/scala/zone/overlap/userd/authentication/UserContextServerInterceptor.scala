1e930788bd1f00f22293c61bbfe8b90998b900c0
Vy-Shane Xie <shane@node.mu>
2018-02-02 20:29:42 +0800

    Fix NPE

--- src/main/scala/zone/overlap/userd/authentication/UserContextServerInterceptor.scala
+++ src/main/scala/zone/overlap/userd/authentication/UserContextServerInterceptor.scala
@@ -75,7 +75,7 @@ case class UserContextServerInterceptor(config: Config) extends ServerIntercepto
 
 object UserContextServerInterceptor {
   val userContextKey: Context.Key[UserContext] = Context.key("user_context")
-  def getUserContext(): Option[UserContext] = Some(userContextKey.get)
+  def getUserContext(): Option[UserContext] = Option(userContextKey.get)
 
   def ensureAuthenticated(): Task[UserContext] = {
     getUserContext()
